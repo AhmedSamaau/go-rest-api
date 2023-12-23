300e6f5c6b09c20cfce67e21b4fde8ac028c195f
AhmedSamaau <ahmedsamaau14@gmail.com>
2023-12-23 22:49:22 +0500

    initial commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+
+	"example.com/event-booking/db"
+	"example.com/event-booking/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db.InitDB()
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080") // localhost:8080
+}
+
+func getEvents(ctx *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events. Try again later."})
+	}
+	ctx.JSON(http.StatusOK, events)
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+	err := ctx.ShouldBindJSON(&event)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	err = event.Save()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event. Try again later."})
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
+}
---
+++ models/event.go
@@ -0,0 +1,59 @@
+package models
+
+import (
+	"time"
+
+	"example.com/event-booking/db"
+)
+
+type Event struct {
+	ID          int64
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserID      int
+}
+
+var events = []Event{}
+
+func (e Event) Save() error {
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES (?, ?, ?, ?, ?)
+	`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	results, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := results.LastInsertId()
+	e.ID = id
+	return err
+}
+
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+	for rows.Next() {
+		var e Event
+		err := rows.Scan(&e.ID, &e.Name, &e.Description, &e.Location, &e.DateTime, &e.UserID)
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, e)
+	}
+
+	return events, nil
+}
